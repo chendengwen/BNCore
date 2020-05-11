ed74fbbe3bea9d18cb9eb860eed92d9236470a81
Gary <596765672@qq.com>
2020-05-11 09:24:54 +0800

    修改UIDeviceExtension

--- BNCore/Classes/Extension/Foundation/UIDeviceExtension.swift
+++ BNCore/Classes/Extension/Foundation/UIDeviceExtension.swift
@@ -14,26 +14,18 @@ let BN_SCREEN_WIDTH:CGFloat = UIScreen.main.bounds.width
 let BN_SCREEN_HEIGHT:CGFloat = UIScreen.main.bounds.height
 
 // 导航栏高度
-let NavHeight:CGFloat = UIDevice.iPhoneX ? 70.0 : 64.0
+let BN_NavHeight:CGFloat = UIDevice.iPhoneX ? 70.0 : 64.0
 
 //tabbar高度
-let TabHeight:CGFloat = UIDevice.iPhoneX ? (49 + 34) : 49
+let BN_TabHeight:CGFloat = UIDevice.iPhoneX ? (49 + 34) : 49
 
-extension UIDevice {
-    
-    // 判断是否为iPhone X 系列 -- 方式一
+public extension UIDevice {
+
+    // 判断是否为刘海屏
     class var iPhoneX: Bool {
-        if BN_SCREEN_HEIGHT == 812 || BN_SCREEN_HEIGHT == 896 {
-            return true
-        }
-        return false
-    }
-    
-    // 判断是否为iPhone X 系列 -- 方式二
-    public func isPhoneX() -> Bool {
         var isPhoneX = false
         if #available(iOS 11.0, *) {
-            isPhoneX = UIApplication.shared.delegate?.window!?.safeAreaInsets.bottom as! CGFloat > 0.0
+            isPhoneX = (UIApplication.shared.delegate?.window!!.safeAreaInsets.bottom)! > 0.0
         }
         return isPhoneX
     }
